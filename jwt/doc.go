232292178322f55ba44be7aaaab1ec957f2e3693
Frank Mueller <mue@tideland.biz>
2016-09-06 19:30:27 +0000

    Fixed newly added tests

--- jwt/doc.go
+++ jwt/doc.go
@@ -23,7 +23,7 @@ import (
 
 // PackageVersion returns the version of the version package.
 func PackageVersion() version.Version {
-	return version.New(2, 0, 0, "beta", "2016-09-05")
+	return version.New(2, 0, 0, "beta", "2016-09-06")
 }
 
 // EOF
