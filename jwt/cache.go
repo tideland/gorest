75035560bac360917873a9a70b9f7787fd5ccd08
Frank Mueller <mue@tideland.biz>
2016-09-06 20:14:02 +0000

    Started implementation of token cache
    The cache shall help to only unmarshall the
    payload once.

---
+++ jwt/cache.go
@@ -0,0 +1,56 @@
+// Tideland Go REST Server Library - JSON Web Token - Cache
+//
+// Copyright (C) 2016 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package jwt
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"sync"
+	"time"
+)
+
+//--------------------
+// CACHE
+//--------------------
+
+// Cache provides a caching for tokens so that these
+// don't have to be decoded or verified multiple times.
+type Cache interface {
+	// Get tries to retrieve a token from the cache.
+	Get(token string) (JWT, bool)
+
+	// Put adds a token to the cache.
+	Put(jwt JWT)
+}
+
+// cacheEntry manages a token and its access time.
+type cacheEntry struct {
+	jwt      JWT
+	accessed time.Time
+}
+
+// cache implements Cache.
+type cache struct {
+	mutex   sync.RWMutex
+	entries map[string]*cacheEntry
+}
+
+func (c *cache) Get(token string) (JWT, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	entry, ok := c.entries[token]
+	if !ok {
+		return nil, false
+	}
+	// TODO Check claims and their validity.
+	return entry.jwt, true
+}
+
+// EOF
