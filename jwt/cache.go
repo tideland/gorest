16a881e6d372a2b2eb92ddf4c5fe909a7a49c82d
themue <mue@tideland.biz>
2016-09-08 13:20:27 +0200

    Further cache implementation
    So far now background cleanup.

--- jwt/cache.go
+++ jwt/cache.go
@@ -38,27 +38,50 @@ type cacheEntry struct {
 
 // cache implements Cache.
 type cache struct {
-	mutex   sync.RWMutex
+	mutex   sync.Mutex
+	cleanup time.Duration
+	leeway  time.Duration
 	entries map[string]*cacheEntry
+}
+
+// NewCache creates a new JWT caching. It takes two
+// durations. The first one is the time a token hasn't
+// been used anymore before it is cleaned up. The second
+// one is the leeway taken for token time validations.
+func NewCache(cleanup, leeway time.Duration) Cache {
+	c := &cache{
+		cleanup: cleanup,
+		leeway:  leeway,
+		entries: map[string]*cacheEntry{},
+	}
+	// TODO Start cleanup goroutine.
+	return c
 }
 
 // Get implements the Cache interface.
 func (c *cache) Get(token string) (JWT, bool) {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	entry, ok := c.entries[token]
 	if !ok {
 		return nil, false
 	}
-	// TODO Check claims and their validity.
-	return entry.jwt, true
+	if entry.jwt.IsValid(c.leeway) {
+		entry.accessed = time.Now()
+		return entry.jwt, true
+	}
+	// Remove invalid token.
+	delete(c.entries, token)
+	return nil, false
 }
 
 // Put implements the Cache interface.
 func (c *cache) Put(jwt JWT) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-	delete(c.entries, jwt.String())
+	if jwt.IsValid(c.leeway) {
+		c.entries[jwt.String()] = &cacheEntry{jwt, time.Now()}
+	}
 }
 
 // EOF
