5d8995dcc926c031f37ddfbc2dc96fa8f8f0da00
themue <mue@tideland.biz>
2016-09-12 10:15:12 +0200

    Renamed header based JWT decode and verify functions

--- jwt/doc.go
+++ jwt/doc.go
@@ -23,7 +23,7 @@ import (
 
 // PackageVersion returns the version of the version package.
 func PackageVersion() version.Version {
-	return version.New(2, 0, 0, "beta", "2016-09-11")
+	return version.New(2, 0, 0, "beta", "2016-09-12")
 }
 
 // EOF
