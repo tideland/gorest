29f289de9720a7c9332007cd17e6a59c31b3e41a
themue <mue@tideland.biz>
2016-10-28 16:17:25 +0200

    Request handling almost finished

--- request/doc.go
+++ request/doc.go
@@ -23,7 +23,7 @@ import (
 
 // Version returns the version of the REST Audit package.
 func Version() version.Version {
-	return version.New(2, 5, 2)
+	return version.New(2, 6, 0, "alpha", "2016-10-28")
 }
 
 // EOF
