9f4cb75b5c25491725d734e8026a84fbd5ee8a6c
Frank Mueller <mue@tideland.biz>
2017-04-24 17:47:54 +0200

    Started adding REST method name dispatching

--- rest/handler.go
+++ rest/handler.go
@@ -41,6 +41,13 @@ type GetResourceHandler interface {
 	Get(job Job) (bool, error)
 }
 
+// ReadResourceHandler is the additional interface for
+// handlers understanding the verb GET but mapping it
+// to method Read() according to the REST conventions.
+type ReadResourceHandler interface {
+	Read(job Job) (bool, error)
+}
+
 // HeadResourceHandler is the additional interface for
 // handlers understanding the verb HEAD.
 type HeadResourceHandler interface {
@@ -53,16 +60,37 @@ type PutResourceHandler interface {
 	Put(job Job) (bool, error)
 }
 
+// UpdateResourceHandler is the additional interface for
+// handlers understanding the verb PUT but mapping it
+// to method Update() according to the REST conventions.
+type UpdateResourceHandler interface {
+	Update(job Job) (bool, error)
+}
+
 // PostResourceHandler is the additional interface for
 // handlers understanding the verb POST.
 type PostResourceHandler interface {
 	Post(job Job) (bool, error)
 }
 
+// CreateResourceHandler is the additional interface for
+// handlers understanding the verb POST but mapping it
+// to method Create() according to the REST conventions.
+type CreateResourceHandler interface {
+	Create(job Job) (bool, error)
+}
+
 // PatchResourceHandler is the additional interface for
 // handlers understanding the verb PATCH.
 type PatchResourceHandler interface {
 	Patch(job Job) (bool, error)
+}
+
+// ModifyResourceHandler is the additional interface for
+// handlers understanding the verb PATCH but mapping it
+// to method Modify() according to the REST conventions.
+type ModifyResourceHandler interface {
+	Modify(job Job) (bool, error)
 }
 
 // DeleteResourceHandler is the additional interface for
@@ -77,8 +105,16 @@ type OptionsResourceHandler interface {
 	Options(job Job) (bool, error)
 }
 
+// InfoResourceHandler is the additional interface for
+// handlers understanding the verb OPTION but mapping it
+// to method Info() according to the REST conventions.
+type InfoResourceHandler interface {
+	Info(job Job) (bool, error)
+}
+
 // handleJob dispatches the passed job to the right method of the
-// passed handler.
+// passed handler. It always tries the nativ method first, then
+// the alias method according to the REST conventions.
 func handleJob(handler ResourceHandler, job Job) (bool, error) {
 	id := func() string {
 		return fmt.Sprintf("%s@%s/%s", handler.ID(), job.Domain(), job.Resource())
@@ -87,7 +123,10 @@ func handleJob(handler ResourceHandler, job Job) (bool, error) {
 	case http.MethodGet:
 		grh, ok := handler.(GetResourceHandler)
 		if !ok {
-			return false, errors.New(ErrNoGetHandler, errorMessages, id())
+			grh, ok = handler.(ReadResourceHandler)
+			if !ok {
+				return false, errors.New(ErrNoGetHandler, errorMessages, id())
+			}
 		}
 		return grh.Get(job)
 	case http.MethodHead:
@@ -99,19 +138,28 @@ func handleJob(handler ResourceHandler, job Job) (bool, error) {
 	case http.MethodPut:
 		prh, ok := handler.(PutResourceHandler)
 		if !ok {
-			return false, errors.New(ErrNoPutHandler, errorMessages, id())
+			prh, ok = handler.(UpdateResourceHandler)
+			if !ok {
+				return false, errors.New(ErrNoPutHandler, errorMessages, id())
+			}
 		}
 		return prh.Put(job)
 	case http.MethodPost:
 		prh, ok := handler.(PostResourceHandler)
 		if !ok {
-			return false, errors.New(ErrNoPostHandler, errorMessages, id())
+			prh, ok = handler.(CreateResourceHandler)
+			if !ok {
+				return false, errors.New(ErrNoPostHandler, errorMessages, id())
+			}
 		}
 		return prh.Post(job)
 	case http.MethodPatch:
 		prh, ok := handler.(PatchResourceHandler)
 		if !ok {
-			return false, errors.New(ErrNoPatchHandler, errorMessages, id())
+			prh, ok = handler.(ModifyResourceHandler)
+			if !ok {
+				return false, errors.New(ErrNoPatchHandler, errorMessages, id())
+			}
 		}
 		return prh.Patch(job)
 	case http.MethodDelete:
@@ -123,7 +171,10 @@ func handleJob(handler ResourceHandler, job Job) (bool, error) {
 	case http.MethodOptions:
 		orh, ok := handler.(OptionsResourceHandler)
 		if !ok {
-			return false, errors.New(ErrNoOptionsHandler, errorMessages, id())
+			orh, ok = handler.(InfoResourceHandler)
+			if !ok {
+				return false, errors.New(ErrNoOptionsHandler, errorMessages, id())
+			}
 		}
 		return orh.Options(job)
 	}
