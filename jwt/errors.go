ea78867898f2c23e7eeb3464b1ea567049b7ae1a
Frank Mueller <mue@tideland.biz>
2017-09-30 23:45:29 +0200

    Finished new retrieving, tests not yet done

--- jwt/errors.go
+++ jwt/errors.go
@@ -39,27 +39,31 @@ const (
 	ErrNoECDSAKey
 	ErrCannotParseRSA
 	ErrNoRSAKey
+	ErrNoAuthorizationHeader
+	ErrInvalidAuthorizationHeader
 )
 
 var errorMessages = errors.Messages{
-	ErrCannotEncode:       "cannot encode the %s",
-	ErrCannotDecode:       "cannot decode the %s",
-	ErrCannotSign:         "cannot sign the token",
-	ErrCannotVerify:       "cannot verify the %s",
-	ErrNoKey:              "no key available, only after encoding or verifying",
-	ErrJSONMarshalling:    "error marshalling to JSON",
-	ErrJSONUnmarshalling:  "error unmarshalling from JSON",
-	ErrInvalidTokenPart:   "part of the token contains invalid data",
-	ErrInvalidCombination: "invalid combination of algorithm %q and key type %q",
-	ErrInvalidAlgorithm:   "signature algorithm %q is invalid",
-	ErrInvalidKeyType:     "key type %T is invalid",
-	ErrInvalidSignature:   "token signature is invalid",
-	ErrCannotReadPEM:      "cannot read the PEM",
-	ErrCannotDecodePEM:    "cannot decode the PEM",
-	ErrCannotParseECDSA:   "cannot parse the ECDSA",
-	ErrNoECDSAKey:         "passed key is no ECDSA key",
-	ErrCannotParseRSA:     "cannot parse the RSA",
-	ErrNoRSAKey:           "passed key is no RSA key",
+	ErrCannotEncode:               "cannot encode the %s",
+	ErrCannotDecode:               "cannot decode the %s",
+	ErrCannotSign:                 "cannot sign the token",
+	ErrCannotVerify:               "cannot verify the %s",
+	ErrNoKey:                      "no key available, only after encoding or verifying",
+	ErrJSONMarshalling:            "error marshalling to JSON",
+	ErrJSONUnmarshalling:          "error unmarshalling from JSON",
+	ErrInvalidTokenPart:           "part of the token contains invalid data",
+	ErrInvalidCombination:         "invalid combination of algorithm %q and key type %q",
+	ErrInvalidAlgorithm:           "signature algorithm %q is invalid",
+	ErrInvalidKeyType:             "key type %T is invalid",
+	ErrInvalidSignature:           "token signature is invalid",
+	ErrCannotReadPEM:              "cannot read the PEM",
+	ErrCannotDecodePEM:            "cannot decode the PEM",
+	ErrCannotParseECDSA:           "cannot parse the ECDSA",
+	ErrNoECDSAKey:                 "passed key is no ECDSA key",
+	ErrCannotParseRSA:             "cannot parse the RSA",
+	ErrNoRSAKey:                   "passed key is no RSA key",
+	ErrNoAuthorizationHeader:      "request contains no authorization header",
+	ErrInvalidAuthorizationHeader: "invalid authorization header: '%s'",
 }
 
 // EOF
